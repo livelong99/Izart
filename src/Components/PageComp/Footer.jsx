8336555f5d9eb6a494983a8c65dea62a702c059e
Vaibhav Chaudhary <vaibhav.189301099@muj.manipal.edu>
2021-03-10 02:35:07 +0530

    nav fix

--- src/Components/PageComp/Footer.jsx
+++ src/Components/PageComp/Footer.jsx
@@ -14,30 +14,17 @@ const Footer = () => {
 			<div class="container">
 				<div class="sec aboutus">
 					<h2>About Us</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-				<ul class="sci">
+					<br/><p>Izart is home to Data-Driven Story Tellers. We are obsessed with helping businesses scale up their operations with the help of premium Content Marketing services. We excel in weaving stories around your brand that convert a passive reader to an active consumer. We understand Data but more importantly, we understand Humans.</p>
+				{/* <ul class="sci">
 					<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
 					<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
 					<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
 					<li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-				</ul>
-			</div>
-			<div class ="sec quick">
-				<h2>Quick Links</h2>
-				<ul class="links">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Services</a></li>
-					<li><a href="#">Resource</a></li>
-					<li><a href="#">Industries</a></li>
-				</ul>
+				</ul> */}
 			</div>
 			<div class ="sec contact">
 				<h2>Contact Info</h2>
 				<ul class="info">
-					<li>
-						<span><i class=" loc fa fa-map-marker" aria-hidden="true"></i></span>
-						<span>Jaipur Express<br/>near Manipal University Jaipur<br/>India</span>
-					</li>
 					<li>
 						<span><i class="fa fa-phone" aria-hidden="true"></i></span>
 						<p><a href="tel:9319150688">+91 9319150688</a></p>
